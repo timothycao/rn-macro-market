74a61f688335674ff0885d156bad5a2fb06714e8
Timothy Cao <tcao.pro@gmail.com>
2020-02-17 19:08:10 -0500

    cart component summary

--- components/shop/Cart.js
+++ components/shop/Cart.js
@@ -1,10 +1,66 @@
 import React from 'react';
 import { View, Text, FlatList, Button, StyleSheet } from 'react-native';
+import { useSelector } from 'react-redux';
+
+import Colors from '../../constants/Colors';
 
 const Cart = props => {
-  return <Text>CART PLACEHOLDER</Text>
+  const cartTotalAmount = useSelector(state => state.cart.totalAmount);
+  const cartItems = useSelector(state => {
+    const cartItemsArray = [];
+    for (const key in state.cart.items) {
+      cartItemsArray.push({
+        productId: key,
+        productTitle: state.cart.items[key].productTitle,
+        productPrice: state.cart.items[key].productPrice,
+        quantity: state.cart.items[key].quantity,
+        totalAmount: state.cart.items[key].totalAmount
+      });
+    }
+    return cartItemsArray;
+  })
+
+  return (
+    <View style={styles.screen}>
+      <View style={styles.summary}>
+        <Text style={styles.summaryText}>
+          Total:{' '}
+          <Text style={styles.amount}>${cartTotalAmount}</Text>
+        </Text>
+        <Button color={Colors.primary} title="Order Now" disabled={cartItems.length === 0} />
+      </View>
+      <View>
+        <Text>CART ITEMS PLACEHOLDER</Text>
+      </View>
+    </View>
+  )
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    margin: 20
+  },
+  summary: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 20,
+    padding: 10,
+    shadowColor: 'black',
+    shadowOpacity: 0.26,
+    shadowOffset: {width: 0, height: 2},
+    shadowRadius: 8,
+    elevation: 5,
+    borderRadius: 5,
+    backgroundColor: 'white'
+  },
+  summaryText: {
+    fontFamily: 'open-sans-bold',
+    fontSize: 18
+  },
+  amount: {
+    color: 'green'
+  }
+});
 
 export default Cart;
