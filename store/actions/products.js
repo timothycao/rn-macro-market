4487215a9c55782d52933720e2e73b916ae91a35
Timothy Cao <tcao.pro@gmail.com>
2020-03-12 20:26:51 -0400

    create product thunk for firebase storage

--- store/actions/products.js
+++ store/actions/products.js
@@ -7,14 +7,32 @@ export const deleteProduct = productId => {
 };
 
 export const createProduct = (title, description, imageUrl, price) => {
-  return {
-    type: CREATE_PRODUCT,
-    productData: {
-      title,
-      description,
-      imageUrl,
-      price
-    }
+  return async dispatch => {
+    const response = await fetch('https://macro-mkt.firebaseio.com/products.json', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        title,
+        description,
+        imageUrl,
+        price
+      })
+    });
+
+    const data = await response.json();
+
+    dispatch({
+      type: CREATE_PRODUCT,
+      productData: {
+        id: data.name,
+        title,
+        description,
+        imageUrl,
+        price
+      }
+    });
   };
 };
 
